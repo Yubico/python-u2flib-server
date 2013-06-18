516871a5ffbfbaff661c77abd86e2cecab4be719
Dain Nilsson <dain@yubico.com>
2013-06-18 15:38:39 +0200

    Added test using multi enroll.

--- test/test_u2f.py
+++ test/test_u2f.py
@@ -87,13 +87,13 @@ def test_enroll_soft_u2f():
 def test_challenge_soft_u2f():
     device = SoftU2FDevice()
     enrollment = u2f.enrollment(ORIGIN)
-    response = device.register(enrollment.json)
+    response = json.dumps(device.register(enrollment.json))
     binding = enrollment.bind(response)
 
     challenge1 = binding.make_challenge()
     challenge2 = binding.make_challenge()
 
-    response2 = device.getAssertion(challenge2.json)
+    response2 = json.dumps(device.getAssertion(challenge2.json))
     response1 = device.getAssertion(challenge1.json)
 
     assert challenge1.validate(response1)
@@ -116,6 +116,17 @@ def test_challenge_soft_u2f():
 
 def test_multi_enroll():
     from u2flib.u2f import enrollment as multi_enroll
+    device1 = SoftU2FDevice()
+    device2 = SoftU2FDevice()
+
     enrollment = multi_enroll(ORIGIN)
-    enroll_req = enrollment.json
-    assert enroll_req
+
+    response1 = device1.register(enrollment.json)
+    response2 = device2.register(enrollment.json)
+
+    response = json.dumps([response1, response2])
+    print response
+
+    bindings = enrollment.bind(response)
+
+    assert len(bindings) == 2
