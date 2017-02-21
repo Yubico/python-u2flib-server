43b091afcb841a727393854522ac1ac704e576fa
Dain Nilsson <dain@yubico.com>
2017-02-21 15:20:30 +0100

    Fix tests.

--- test/test_model.py
+++ test/test_model.py
@@ -1,31 +1,3 @@
-# Copyright (c) 2017 Yubico AB
-# All rights reserved.
-#
-#   Redistribution and use in source and binary forms, with or
-#   without modification, are permitted provided that the following
-#   conditions are met:
-#
-#    1. Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#    2. Redistributions in binary form must reproduce the above
-#       copyright notice, this list of conditions and the following
-#       disclaimer in the documentation and/or other materials provided
-#       with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
-
 from u2flib_server.utils import websafe_decode
 from u2flib_server.model import (JSONDict, RegistrationData, SignatureData,
                                  U2fRegisterRequest, U2fSignRequest)
@@ -158,6 +130,8 @@ class U2fRegisterRequestTest(unittest.TestCase):
             }]
         }), req)
 
+        self.assertEqual(U2fRegisterRequest.wrap(req.json), req)
+
         self.assertEqual(
             websafe_decode('EAaArVRs5qV39C9S3zO0z9ynVoWeZkuNfeMpsVDQnOk'),
             req.applicationParameter
@@ -186,6 +160,7 @@ class U2fSignRequestTest(unittest.TestCase):
                 "challenge": challenge
              }
         )
+        self.assertEqual(U2fSignRequest.wrap(req.json), req)
         self.assertEqual(
             req.applicationParameter,
             websafe_decode('EAaArVRs5qV39C9S3zO0z9ynVoWeZkuNfeMpsVDQnOk')
